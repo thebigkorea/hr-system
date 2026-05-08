54c2b54a75e3c2fa36b7f1a14bf90112d28edc80
본인이름 <>
2026-05-08 15:02:13 +0900

    replace certificate full files

--- certificate.js
+++ certificate.js
@@ -82,10 +82,8 @@ function fillCertificate(emp, purpose, issueNo) {
 
 function getTodayKorean() {
   const today = new Date();
-
   const y = today.getFullYear();
   const m = String(today.getMonth() + 1).padStart(2, "0");
   const d = String(today.getDate()).padStart(2, "0");
-
   return `${y}년 ${m}월 ${d}일`;
 }
